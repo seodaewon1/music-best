eebe1b533bf2e6986d055febf74f6bff57fa2e05
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-04-30 09:52:50 +0900

    Update pyMelonChart.py

--- pyMelonChart.py
+++ pyMelonChart.py
@@ -1,1 +1,33 @@
+import requests as req
+from bs4 import BeautifulSoup as bs
+import pandas as pd
 
+head = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
+res = req.get("https://www.melon.com/chart/index.htm", headers=head)
+soup = bs(res.text, "lxml")
+
+ranking = soup.select("tbody .wrap.t_center > .rank")
+title = soup.select("tbody .wrap_song_info .ellipsis.rank01 span > a")
+artist = soup.select("tbody .wrap_song_info .ellipsis.rank02 span > a:nth-child(1)")
+
+
+rankingList = []
+titleList = []
+artistList = []
+
+print(len(ranking))
+print(len(artist))
+print(len(title))
+
+rankingList = [r.text.strip() for r in ranking]
+titleList = [t.text.strip() for t in title]
+artistList = [a.text.strip() for a in artist]
+
+chart_df = pd.DataFrame({
+    'Ranking': rankingList,
+    'Title': titleList,
+    'Artist': artistList
+})
+
+# JSON 파일로 저장 
+chart_df.to_json("MelonChart100.json", force_ascii=False, orient="records")
