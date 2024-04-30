46c3daf37fdb06e36db3d96d5fcf977ac0bae07a
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-04-30 10:07:29 +0900

    Update pyMelonChart.py

--- pyMelonChart.py
+++ pyMelonChart.py
@@ -30,4 +30,4 @@ chart_df = pd.DataFrame({
 })
 
 # JSON 파일로 저장 
-chart_df.to_json("MelonChart100.json", force_ascii=False, orient="records")
+chart_df.to_json("melon/MelonChart100.json", force_ascii=False, orient="records")
