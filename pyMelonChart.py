46f2623c7b31d0b8148f16ae5114393660423d44
seodaewon1 <162085705+seodaewon1@users.noreply.github.com>
2024-04-30 10:20:13 +0900

    Update pyMelonChart.py

--- pyMelonChart.py
+++ pyMelonChart.py
@@ -1,49 +1,33 @@
 import requests as req
 from bs4 import BeautifulSoup as bs
-import json
-import datetime
+import pandas as pd
 
-# 현재 날짜를 문자열로 저장
-current_date = datetime.datetime.now().strftime("%Y-%m-%d")
-
-head = {'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
+head = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36'}
 res = req.get("https://www.melon.com/chart/index.htm", headers=head)
-
-# print(res.text)
-# print(res.status_code)  #406
-
 soup = bs(res.text, "lxml")
 
-# 데이터 선택
 ranking = soup.select("tbody .wrap.t_center > .rank")
 title = soup.select("tbody .wrap_song_info .ellipsis.rank01 span > a")
 artist = soup.select("tbody .wrap_song_info .ellipsis.rank02 span > a:nth-child(1)")
-image = soup.select("tbody .image_typeAll > img")
-album = soup.select("tbody .wrap_song_info .ellipsis.rank03 > a")
 
-# print(len(image))
 
-# 데이터 저장
-rankings = [r.text.strip() for r in ranking]
-titles = [t.text.strip() for t in title]
-artists = [a.text.strip() for a in artist]
-images = [i.get('src') for i in image]
-albums = [a.text.strip() for a in album]
+rankingList = []
+titleList = []
+artistList = []
 
-# 데이터 프레임 생성
-chart_data = []
-for ranking, title, artist, image_url, album in zip(rankings, titles, artists, images, albums):
-    chart_data.append({
-        "rank": ranking,
-        "title": title,
-        "artist": artist,
-        "imageURL": image_url,
-        "album": album
-    })
+print(len(ranking))
+print(len(artist))
+print(len(title))
 
-# 파일 이름 설정
-file_name = f"melon/melon100_{current_date}.json"
+rankingList = [r.text.strip() for r in ranking]
+titleList = [t.text.strip() for t in title]
+artistList = [a.text.strip() for a in artist]
 
-# JSON 파일로 저장
-with open(file_name, 'w', encoding='utf-8') as file:
-    json.dump(chart_data, file, ensure_ascii=False, indent=4)
+chart_df = pd.DataFrame({
+    'Ranking': rankingList,
+    'Title': titleList,
+    'Artist': artistList
+})
+
+# JSON 파일로 저장 
+chart_df.to_json("MelonChart100.json", force_ascii=False, orient="records")
